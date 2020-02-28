a55af696d2a9c3fcb439106a4a4ea16c5d017716
Thom Wise <thom.gkw.wp@gmail.com>
2020-02-28 11:51:30 -0800

    Made the banner image responsive and dynamic

--- partials/partials-banner.php
+++ partials/partials-banner.php
@@ -1,5 +1,5 @@
 <section>
-  <div class="banner darken-photo" style="background: url(https://api.time.com/wp-content/uploads/2019/08/balloon-destroyer-viral-video-twitter-jan-erichson.jpg);">
+  <div class="banner darken-photo" style="background-image: url(<?php the_post_thumbnail_url(); ?>);">
     <h1><?php the_title(); ?></h1>
   </div>
 </section>
