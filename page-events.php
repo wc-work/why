b0dcef1273f3c53d81dfaab3b4dcd4f7a111cede
Thom Wise <thom.gkw.wp@gmail.com>
2020-02-28 11:28:04 -0800

    Added banner with static background photo to events page

--- page-events.php
+++ page-events.php
@@ -1,28 +1,25 @@
-<?php get_header(); ?>
+<?php
+/*
+Template Name: Events
+Template Post Type: page
+*/
+ get_header(); ?>
 
 	<main role="main">
 		<!-- section -->
 		<section>
 
-			<h1><?php the_title(); ?>Hi</h1>
 
 		<?php if (have_posts()): while (have_posts()) : the_post(); ?>
+            <!-- banner -->
+            <?php get_template_part('./partials/partials-banner', 'banner-section'); ?>
+            <!-- /banner -->
 
 			<!-- article -->
-			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-
-				<?php the_content(); ?>
-
-				<?php comments_template( '', true ); // Remove if you don't want comments ?>
-
-				<br class="clear">
-
-				<?php edit_post_link(); ?>
-
-			</article>
+			
 			<!-- /article -->
 
-		<?php endwhile; ?>
+        <?php endwhile; ?>
 
 		<?php else: ?>
 
---
+++ partials/partials-banner.php
@@ -0,0 +1,5 @@
+<section>
+  <div class="banner darken-photo" style="background: url(https://api.time.com/wp-content/uploads/2019/08/balloon-destroyer-viral-video-twitter-jan-erichson.jpg);">
+    <h1><?php the_title(); ?></h1>
+  </div>
+</section>
