cecb09e35ce1f2b3c84951eaf901e6e184cd7b9f
Robert Nelson <robnelson@Roberts-MacBook-Pro.local>
2020-03-10 14:24:24 -0700

    Home Page Content: added color to content boxes and dummy content, with photo.

--- page-home.php
+++ page-home.php
@@ -7,7 +7,7 @@ Template Post Type: page
 get_header(); ?>
 
 <main class="page-home">
-  <div class="carousel-wrapper container">
+  <div class="carousel-wrapper container-fluid home-slider">
     <div class="carousel">
       <img class="carousel__photo initial" src="<?php echo get_field('picture_1')['url'] ?>">
       <img class="carousel__photo" src="<?php echo get_field('picture_2')['url'] ?>">
@@ -22,7 +22,7 @@ get_header(); ?>
 
   <!-------------------------Carousel Text ------------------------>
 
-  <!-------------------------- ------------------------------------>
+  <!-------------------------- Our Story Content------------------------------------>
 
   <section class="container home-content rounded">
     <div class="row d-flex justify-content-center mt-3">
@@ -30,10 +30,24 @@ get_header(); ?>
     </div>
     <!-- <div class="info-cards-wrapper"> -->
       <div class="d-flex content centered">
-        <div class="col row-one box-one box">Box</div>
-        <div class="col row-one box-two box">Box</div>
-        <div class="col row-two box-three box">Box</div>
-        <div class="col row-two box-four box">Box</div>
+        <div class="col row-one box-one box">
+        <img class="team-photo" src="<?php echo get_field('our_story_photo')['url'] ?>">
+        </div>
+        <div class="col row-one box-two box">
+          <h2>History</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto explicabo, autem unde sint commodi rem iure repellat fugit minus non distinctio ipsum, voluptates ab. Eveniet, tempora vitae a amet odit fuga tempore. Minus, error, delectus excepturi molestiae laboriosam consectetur provident optio expedita laudantium earum laborum. Deleniti, eveniet quisquam?
+          </p>
+        </div>
+        <div class="col row-two box-three box">
+          <h2>What We Do</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto explicabo, autem unde sint commodi rem iure repellat fugit minus non distinctio ipsum, voluptates ab. Eveniet, tempora vitae a amet odit fuga tempore. Minus, error, delectus excepturi molestiae laboriosam consectetur provident optio expedita laudantium earum laborum. Deleniti, eveniet quisquam?
+          </p>
+        </div>
+        <div class="col row-two box-four box">
+          <h2>Why Give to W.H.Y.</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto explicabo, autem unde sint commodi rem iure repellat fugit minus non distinctio ipsum, voluptates ab. Eveniet, tempora vitae a amet odit fuga tempore. Minus, error, delectus excepturi molestiae laboriosam consectetur provident optio expedita laudantium earum laborum. Deleniti, eveniet quisquam?
+          </p>
+        </div>
       </div>
     <!-- </div> -->
   </section>
