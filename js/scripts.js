aacebb24ca37946545f0475fbe31190c030378cc
Thom Wise <thom.gkw.wp@gmail.com>
2020-03-26 13:47:29 -0700

    Fixed nesting errors in scripts.js thanks to Rob

--- js/scripts.js
+++ js/scripts.js
@@ -7,30 +7,26 @@
 		(function($){
 			$(document).ready(function(){
 			
-			$(function() {	
-			  var menu = $('.cssmenu > ul');
+				$(function() {	
+					var menu = $('.cssmenu > ul');
 
-			  menu.find('.has-sub > ul').hide();
+					menu.find('.has-sub > ul').hide();
 
-			  $( '#toggle' ).click(function() {
-				$( this ).toggleClass( "curves" );
-			  });
-			
-			  menu.on('click', function(event) {
-				event.preventDefault();
+					$( '#toggle' ).click(function() {
+						$( this ).toggleClass( "curves" );
+					});
+					
+					menu.on('click', function(event) {
+						event.preventDefault();
 
-				var targetParent = $(event.target).parent();
-				if (targetParent.hasClass('has-sub')) {
-				  targetParent.toggleClass('active');
-				  targetParent.children('ul').slideToggle(250);
-				} 
-			  })
+						var targetParent = $(event.target).parent();
+						if (targetParent.hasClass('has-sub')) {
+							targetParent.toggleClass('active');
+							targetParent.children('ul').slideToggle(250);
+						} 
+					})
+				});
 			});
-			
-			});
-			})(jQuery);
-			
-		
-	});
-	
+		})(jQuery);	
+	});	
 })(jQuery, this);
