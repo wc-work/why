38690bcc6a66557f92c89a0e5191f592b4202eb7
Thom Wise <thom.gkw.wp@gmail.com>
2020-03-27 12:07:14 -0700

    Added better navbar styling and mobile responsive features to events page as well

--- page-events.php
+++ page-events.php
@@ -49,57 +49,52 @@ $past_event = new WP_Query($args2);
 
 get_header(); ?>
 
-	<main role="main">
-		<!-- section -->
-		<section>
-
-		
-		<?php if (have_posts()): while (have_posts()) : the_post(); ?>
-            <!-- banner -->
-            <?php get_template_part('./partials/partials-banner', 'banner-section'); ?>
-			<!-- /banner -->
-		<?php endwhile; 
+<main role="main">
+	<?php if (have_posts()): while (have_posts()) : the_post(); ?>
+	<!-- banner -->
+		<?php get_template_part('./partials/partials-banner', 'banner-section'); ?>
+	<!-- /banner -->
+	<?php endwhile; 
 			wp_reset_postdata();
 		endif; ?>
-
-            <!-- Upcoming Events -->
-            <article class="container">
-				<div class="row">
-					<div class="col pt-4 d-flex justify-content-center">
-						<h2>Upcoming Events</h2>
-					</div>
-				</div>	
-				<div class="row">
+	<section>
+		<!-- Upcoming Events -->
+		<article class="container">
+			<div class="row">
+				<div class="col pt-4 d-flex justify-content-center">
+					<h2>Upcoming Events</h2>
+				</div>
+			</div>
+			<div class="row">
 				<?php if($upcoming_event->have_posts() ) : while ( $upcoming_event->have_posts() ) : $upcoming_event->the_post(); ?>
-					<?php get_template_part('./partials/partials-events-top-card', 'top-card'); ?>
+				<?php get_template_part('./partials/partials-events-top-card', 'top-card'); ?>
 				<?php endwhile; 
 					wp_reset_postdata();
-				endif; ?>	
+				endif; ?>
 
+			</div>
+		</article>
+		<!-- /Upcoming Events -->
+
+		<!-- Past Events -->
+		<article class="container">
+			<div class="row">
+				<div class="col pt-4 d-flex justify-content-center">
+					<h2>Past Events</h2>
 				</div>
-			</article>
-			<!-- /Upcoming Events -->
-	
-			<!-- Past Events -->
-			<article class="container">
-				<div class="row">
-					<div class="col pt-4 d-flex justify-content-center">
-						<h2>Past Events</h2>
-					</div>
-				</div>	
-                <div class="row">
+			</div>
+			<div class="row">
 				<?php if($past_event->have_posts() ) : while ( $past_event->have_posts() ) : $past_event->the_post(); ?>
-					<?php get_template_part('./partials/partials-events-bottom-card', 'bottom-card'); ?>
+				<?php get_template_part('./partials/partials-events-bottom-card', 'bottom-card'); ?>
 				<?php endwhile; 
 					wp_reset_postdata();
-				endif; ?>	
+				endif; ?>
 
-				</div>
-			</article>
-			<!-- /Past Events -->
-		
-		</section>
-		<!-- /section -->
-	</main>
+			</div>
+		</article>
+		<!-- /Past Events -->
+	</section>
+	<!-- /section -->
+</main>
 
 <?php get_footer(); ?>
