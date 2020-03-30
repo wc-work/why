5aad7afd9259279619d96d9dd1b9cd95c9bfdb7b
Thom Wise <thom.gkw.wp@gmail.com>
2020-03-30 13:25:38 -0700

    Fixed style errors on mobile navbar moved sass resource file to pages folder instead of partials

--- header.php
+++ header.php
@@ -60,7 +60,7 @@
 							'container'         => 'div',
 							'container_class'   => 'collapse navbar-collapse',
 							'container_id'      => 'bs-example-navbar-collapse-1',
-							'menu_class'        => 'nav navbar-nav ml-auto',
+							'menu_class'        => 'nav navbar-nav',
 							'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
 							'walker'            => new WP_Bootstrap_Navwalker(),
 						) );
